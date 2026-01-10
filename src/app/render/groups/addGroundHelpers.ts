ba02b67946c941f2f7aa78119fe4bac0ec9edec5
xskutsu <xsku@protonmail.com>
2026-01-10 14:52:15 -0600

    Improve ground

--- src/app/render/groups/addGroundHelpers.ts
+++ src/app/render/groups/addGroundHelpers.ts
@@ -2,15 +2,15 @@ import { AxesHelper, GridHelper, type Group } from "three";
 
 export function addGroundHelpers(group: Group): void {
 	const gridHelper1 = new GridHelper(3, 3, 0x444444, 0x444444);
-	gridHelper1.position.y -= 0.5;
+	gridHelper1.position.y = -0.503;
 	group.add(gridHelper1);
 
 	const gridHelper2 = new GridHelper(1, 16, 0x444444, 0x444444);
-	gridHelper2.position.y -= 0.5;
+	gridHelper2.position.y = -0.502;
 	group.add(gridHelper2);
 
 	const axesHelper = new AxesHelper(0.5);
 	axesHelper.scale.set(1, 0, 1);
-	axesHelper.position.y = -0.499;
+	axesHelper.position.y = -0.501;
 	group.add(axesHelper);
 }
