593fcc68a07dae05b801cb08bd33cfc104b0ec40
xskutsu <xsku@protonmail.com>
2026-01-15 21:38:44 -0600

    More closely match ground helpers from Blockbench

--- src/app/render/groups/addGroundHelpers.ts
+++ src/app/render/groups/addGroundHelpers.ts
@@ -2,15 +2,15 @@ import { AxesHelper, GridHelper, type Group } from "three";
 
 export function addGroundHelpers(group: Group): void {
 	const gridHelper1 = new GridHelper(3, 3, 0x444444, 0x444444);
-	gridHelper1.position.y = -0.503;
+	gridHelper1.position.y = -0.5;
 	group.add(gridHelper1);
 
 	const gridHelper2 = new GridHelper(1, 16, 0x444444, 0x444444);
-	gridHelper2.position.y = -0.502;
+	gridHelper2.position.y = -0.5;
 	group.add(gridHelper2);
 
 	const axesHelper = new AxesHelper(0.5);
 	axesHelper.scale.set(1, 0, 1);
-	axesHelper.position.y = -0.501;
+	axesHelper.position.y = -0.4999;
 	group.add(axesHelper);
 }
